93d2fe2495d2aa08b12e3df6151d0a62a06c3768
Dennis <nndenninn@gmail.com>
2020-05-02 08:23:16 +0300

    Start of a new project

--- Bullseye-iOS13/ViewController.swift
+++ Bullseye-iOS13/ViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var slider: UISlider!
+    var currentValue: Int = 15
+    var targetValue = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        startNewRound()
     }
-
-
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        currentValue = lroundf(slider.value)
+    }
+    
+    @IBAction func showAlert(_ sender: UIButton) {
+        alert()
+        startNewRound()
+    }
+    
+    func alert() {
+        let message = "The value of the slider is: \(currentValue)" + "\nThe target value is: \(targetValue)"
+        let alert = UIAlertController(title: "Hello, World", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func startNewRound() {
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+    }
 }
 
